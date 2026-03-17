13bd1473486e2124a11e6bd900171f05299e3fe6
Sali459 <saadiaaali2002@gmail.com>
2026-03-17 13:02:58 +1100

    Added basic HTML

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
-          This project is created by Saadia Ali and is open-sources on{" "}
+          This project is created by Saadia Ali and is open-sourced on{" "}
           <a
             href="https://github.com/Sali459/react-weather-app"
             target="
