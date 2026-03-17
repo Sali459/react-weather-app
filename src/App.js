aced7123a1a8933476fd3556368837c72d9cf8c9
Sali459 <saadiaaali2002@gmail.com>
2026-03-17 12:37:10 +1100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project is created by Saadia Ali and is open-sources on {" "} 
+      <a href="https://github.com/Sali459/react-weather-app" target="
+      _blank">GitHub</a>
+      </footer>
     </div>
   );
 }
