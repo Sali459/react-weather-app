7e2964a7cb3ef589c9ed5e47cb00559a8d9ecd1e
Sali459 <saadiaaali2002@gmail.com>
2026-03-18 12:10:09 +1100

    Added the current date and time

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Weather from "./Weather";
-import './App.css';
-
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York"/>
+        <Weather defaultCity="New York" />
         <footer>
           This project is created by Saadia Ali and is open-sourced on{" "}
           <a
             href="https://github.com/Sali459/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             GitHub
           </a>
--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 
@@ -9,7 +10,7 @@ export default function Weather(props) {
     setWeatherData({
       ready: true,
       city: response.data.city,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
       iconURL:
         "https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg",
@@ -44,7 +45,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
